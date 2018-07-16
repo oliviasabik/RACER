ce65096558f464e500b4b27475af24714467e08b
ols5fg <ols5fg@virginia.edu>
2018-07-16 00:41:07 -0400

    replacing 1000k genomes phase I with phase III

--- R/mirror_plot.R
+++ R/mirror_plot.R
@@ -1,76 +1,206 @@
 #' Mirror Plot Function
 #'
 #' This function allows you to creat a plot of -log10(P-values).
-#' @param assoc_data1 object in your current R environment that has
-#' columns named CHR_POS representing the position of the snp on
-#' the chromosome, LOG10P which contains the -log10(P-values), and optional
-#' columns CHR, which contains the chromosome number, RS_ID, which
-#' contains the rsID numbers of the SNPs, and R2, which contains LD information.
+#' @param assoc_data1 object that has columns named
+#' CHR_POS representing the position of the snp on
+#' the chromosome, LOG10P which contains the -log10(P-values),
+#' CHR, which contains the chromosome number, RS_ID, which
+#' contains the rsID numbers of the SNPs, and R2, which contains
+#' LD information. If you want to compute LD information for the dataset,
+#' add a column to the input called LD, and fill it with NA, and include the
+#' rs_id numbers for the lead SNPs you want to use to compute the LD.
+#'
 #' @param assoc_data2 identifcal in format to assoc_data1
 #' @param name1 name of association set 1
 #' @param name2 name of association set 2
-#' @param x1 lower bound on x axis
-#' @param x2 upper bound on x axis
+#' @param start lower bound on x axis
+#' @param end upper bound on x axis
+#' @param pops populations used to calculate LD
+#' @param rs_id_1 lead snp used to calculate LD for first association dataset
+#' @param rs_id_2 lead snp used to calculate LD for second association dataset
+#'
 #' @keywords association plot
 #' @export
 #' @examples
-#' base_plot_function()
+#' mirror_plot_function(assoc_data1, assoc_data2, "plot1", "plot2", 1000000, 110000000, pops = c("CEU","TSI","FIN","GBR","IBS"), rs_id1 = "rs123456", rs_id2 = "rs123456")
 
-mirror_plot_function <- function(assoc_data1, assoc_data2, name1, name2, x1, x2){
-  # read and process association data set 1
+mirror_plot_function <- function(assoc_data1, assoc_data2, name1, name2, start, end, pops, rs_id_1, rs_id_2){
+  print("Calculating LD")
+  if(!is.null(pops)){
+    if(length(pops) == 1){
+      ld_command_1 = paste0("curl -k -X GET 'https://analysistools.nci.nih.gov/LDlink/LDlinkRest/ldproxy?var=", rs_id_1,
+                          "&pop=",pops,"&r2_d=r2'")
+      z_1 = system(ld_command_1, intern = TRUE)
+      z_1 = as.data.frame(z_1)
+      z_1 = tidyr::separate(z_1, 1, into = c("col_1", "col_2", "col_3", "col_4", "col_5", "col_6", "col_7", "col_8",
+                                         "col_9", "col_10"), sep = "\t")
+      colnames(z_1) = z_1[1,]
+      z_1 = z_1[-1,]
+      z_1 = dplyr::select(z_1, RS_Number, R2)
+      colnames(z_1) = c("RS_ID", "LD")
+      assoc_data1$LD = NA
+      assoc_data1 = dplyr::select(assoc_data1, -LD)
+      assoc_data1 = merge(assoc_data1, z_1, by = "RS_ID", all.x = TRUE)
+    }else if(length(pops) > 1){
+      ld_command_1 = paste0("curl -k -X GET 'https://analysistools.nci.nih.gov/LDlink/LDlinkRest/ldproxy?var=", rs_id_1,
+                          "&pop=")
+      for (i in 1:length(pops)){
+        if (i < length(pops)){
+          print(i)
+          a = pops[i]
+          ld_command_1 = paste0(ld_command_1, a, "%2B")
+        } else if(i == length(pops)){
+          a = pops[i]
+          ld_command_1 = paste0(ld_command_1, a)
+        }
+      }
+      ld_command_1 = paste0(ld_command_1, "&r2_d=r2'")
+      z_1 = system(ld_command_1, intern = TRUE)
+      z_1 = as.data.frame(z_1)
+      z_1 = tidyr::separate(z_1, 1, into = c("col_1", "col_2", "col_3", "col_4", "col_5", "col_6", "col_7", "col_8",
+                                         "col_9", "col_10"), sep = "\t")
+      colnames(z_1) = z_1[1,]
+      z_1 = z_1[-1,]
+      z_1 = dplyr::select(z_1, RS_Number, R2)
+      colnames(z_1) = c("RS_ID", "LD")
+      assoc_data1$LD = NA
+      assoc_data1 = dplyr::select(assoc_data1, -LD)
+      assoc_data1 = merge(assoc_data1, z_1, by = "RS_ID", all.x = TRUE)
+    }
+  }
+
+  if(!is.null(pops)){
+    if(length(pops) == 1){
+      ld_command_2 = paste0("curl -k -X GET 'https://analysistools.nci.nih.gov/LDlink/LDlinkRest/ldproxy?var=", rs_id_1,
+                            "&pop=",pops,"&r2_d=r2'")
+      z_2 = system(ld_command_2, intern = TRUE)
+      z_2 = as.data.frame(z_2)
+      z_2 = tidyr::separate(z_2, 1, into = c("col_1", "col_2", "col_3", "col_4", "col_5", "col_6", "col_7", "col_8",
+                                             "col_9", "col_10"), sep = "\t")
+      colnames(z_2) = z_2[1,]
+      z_2 = z_2[-1,]
+      z_2 = dplyr::select(z_2, RS_Number, R2)
+      colnames(z_2) = c("RS_ID", "LD")
+      assoc_data2$LD = NA
+      assoc_data2 = dplyr::select(assoc_data2, -LD)
+      assoc_data2 = merge(assoc_data2, z_2, by = "RS_ID", all.x = TRUE)
+    }else if(length(pops) > 1){
+      ld_command_2 = paste0("curl -k -X GET 'https://analysistools.nci.nih.gov/LDlink/LDlinkRest/ldproxy?var=", rs_id_1,
+                            "&pop=")
+      for (i in 1:length(pops)){
+        if (i < length(pops)){
+          print(i)
+          a = pops[i]
+          ld_command_2 = paste0(ld_command_2, a, "%2B")
+        } else if(i == length(pops)){
+          a = pops[i]
+          ld_command_2 = paste0(ld_command_2, a)
+        }
+      }
+      ld_command_2 = paste0(ld_command_2, "&r2_d=r2'")
+      z_2 = system(ld_command_2, intern = TRUE)
+      z_2 = as.data.frame(z_2)
+      z_2 = tidyr::separate(z_2, 1, into = c("col_1", "col_2", "col_3", "col_4", "col_5", "col_6", "col_7", "col_8",
+                                             "col_9", "col_10"), sep = "\t")
+      colnames(z_2) = z_2[1,]
+      z_2 = z_2[-1,]
+      z_2 = dplyr::select(z_2, RS_Number, R2)
+      colnames(z_2) = c("RS_ID", "LD")
+      assoc_data2$LD = NA
+      assoc_data2 = dplyr::select(assoc_data2, -LD)
+      assoc_data2 = merge(assoc_data2, z_2, by = "RS_ID", all.x = TRUE)
+    }
+  }
+
+  # read in, format, and filter data sets
+  print("Reading in association data")
   in.dt <- as.data.frame(assoc_data1)
   in.dt$CHR_POS = as.numeric(as.character(in.dt$CHR_POS))
   in.dt$LOG10P = as.numeric(as.character(in.dt$LOG10P))
-  in.dt$LD = as.numeric(as.character(in.dt$LD))
-  in.dt$LD_BIN <- cut(in.dt$LD,
-                      breaks=c(0,0.2,0.4,0.6,0.8,1.0),
-                      labels=c("0.0-0.2","0.2-0.4","0.4-0.6","0.6-0.8","0.8-1.0"))
-  in.dt$LD_BIN = as.character(in.dt$LD_BIN)
-  in.dt$LD_BIN[is.na(in.dt$LD_BIN)] <- "NA"
-  in.dt$LD_BIN = as.factor(in.dt$LD_BIN)
+  in.dt$CHR = as.numeric(as.character(in.dt$CHR))
+  chr_1 = min(in.dt$CHR)
+  in.dt = dplyr::filter(in.dt, CHR_POS > start)%>%
+    dplyr::filter(CHR_POS < end)
 
-  # read and process association data set 2
   in.dt.2 <- as.data.frame(assoc_data2)
   in.dt.2$CHR_POS = as.numeric(as.character(in.dt.2$CHR_POS))
   in.dt.2$LOG10P = as.numeric(as.character(in.dt.2$LOG10P))
+  in.dt.2$CHR = as.numeric(as.character(in.dt.2$CHR))
+  chr_2 = min(in.dt.2$CHR)
+  in.dt.2= dplyr::filter(in.dt.2, CHR_POS > start)%>%
+    dplyr::filter(CHR_POS < end)
+
+  # process ld info
+  in.dt$LD = as.numeric(as.character(in.dt$LD))
+  in.dt$LD_BIN <- cut(in.dt$LD,
+                      breaks=c(0,0.2,0.4,0.6,0.8,1.0),
+                      labels=c("0.2-0.0","0.4-0.2","0.6-0.4","0.8-0.6","1.0-0.8"))
+  in.dt$LD_BIN = as.character(in.dt$LD_BIN)
+  in.dt$LD_BIN[is.na(in.dt$LD_BIN)] <- "NA"
+  in.dt$LD_BIN = as.factor(in.dt$LD_BIN)
+  in.dt$LD_BIN = factor(in.dt$LD_BIN, levels = c("1.0-0.8", "0.8-0.6", "0.6-0.4", "0.4-0.2", "0.2-0.0", "NA"))
+  print(table(in.dt$LD_BIN))
+
+
   in.dt.2$LD = as.numeric(as.character(in.dt.2$LD))
   in.dt.2$LD_BIN <- cut(in.dt.2$LD,
-                      breaks=c(0,0.2,0.4,0.6,0.8,1.0),
-                      labels=c("0.0-0.2","0.2-0.4","0.4-0.6","0.6-0.8","0.8-1.0"))
+                        breaks=c(0,0.2,0.4,0.6,0.8,1.0),
+                        labels=c("0.2-0.0","0.4-0.2","0.6-0.4","0.8-0.6","1.0-0.8"))
   in.dt.2$LD_BIN = as.character(in.dt.2$LD_BIN)
   in.dt.2$LD_BIN[is.na(in.dt.2$LD_BIN)] <- "NA"
   in.dt.2$LD_BIN = as.factor(in.dt.2$LD_BIN)
+  in.dt.2$LD_BIN = factor(in.dt.2$LD_BIN, levels = c("1.0-0.8", "0.8-0.6", "0.6-0.4", "0.4-0.2", "0.2-0.0", "NA"))
+  print(table(in.dt.2$LD_BIN))
 
-  # filter out region requested
-  in.dt = dplyr::filter(in.dt, CHR_POS > x1)%>%
-    dplyr::filter(CHR_POS < x2)
-  in.dt.2= dplyr::filter(in.dt.2, CHR_POS > x1)%>%
-    dplyr::filter(CHR_POS < x2)
-
- # generate mirror plot
+  # reading in gene data
+  data(biomart_hg19)
+  chr_2
+  colnames(biomart_hg19) = c("GENE_ID", "CHR", "TRX_START", "TRX_END", "GENE_NAME", "LENGTH")
+  gene = subset(biomart_hg19, biomart_hg19$CHR == chr_2)
+  gene = subset(gene, gene$TRX_START > (start-5000))
+  gene = subset(gene, gene$TRX_END < (end+5000))
+  gene = arrange(gene, desc(LENGTH))
+  gene = gene[!duplicated(gene$GENE_ID),]
+  gene = gene[,c(3,4,5)]
+  gene = reshape2::melt(gene)
+  gene$y_value = as.numeric(as.factor(gene$GENE_NAME))
+  plot_lab = subset(gene, gene$variable == "TRX_END")
+  c = ggplot(gene, aes(x = value, y = y_value)) +
+    geom_line(aes(group = GENE_NAME), size = 2) + theme_bw() +
+    geom_text(data = plot_lab, aes(x = value, y = y_value, label = GENE_NAME),
+              hjust = -0.1,vjust = 0.3, size = 2.5) + xlim(start,end) +
+    theme(axis.title.y = element_text(color = "white", size = 28),
+          axis.text.y = element_blank(),
+          axis.ticks.y = element_blank()) + xlab(paste0("Chromosome ", chr_1, " Position")) +
+    ylim(0,(max(gene$y_value)+1))
+  # generate mirror plot
+  print("Generating plots")
   a = ggplot2::ggplot(data = in.dt, aes(x = CHR_POS, y = LOG10P, color = LD_BIN)) +
     geom_point() + scale_colour_manual(
-      values = c("navyblue", "skyblue1", "green1", "darkorange1", "red", "grey")) +
-    theme_bw() + xlab("Chromosome Position") + ylab("-log10(p-value)") +
+      values = c("red", "darkorange1", "green1", "skyblue1", "navyblue", "grey")) +
+    theme_bw() + xlab(paste0("Chromosome ", chr_1, " Position")) + ylab("-log10(p-value)") +
     scale_y_reverse() + theme(axis.title.x=element_blank(),
                              axis.text.x=element_blank(),
                              axis.ticks.x=element_blank()) +
     theme(legend.position = "none") +
     #annotate("text", x=((min(in.dt$CHR_POS)+max(in.dt$CHR_POS))/5), y=(max(in.dt$LOG10P)-0.1), label = name1, size = 4) +
-    xlim(min(in.dt$CHR_POS),max(in.dt$CHR_POS)) + ggtitle(paste0("Mirror Plot of ", name1, " and ", name2 ))
+    xlim(start,end) + ggtitle(paste0("Mirror Plot of ", name1, " and ", name2 ))
 
 
   b = ggplot2::ggplot(data = in.dt.2, aes(x = CHR_POS, y = LOG10P, color = LD_BIN)) +
     geom_point() + scale_colour_manual(
-      values = c("navyblue", "skyblue1", "green1", "darkorange1", "red", "grey")) +
-    theme_bw() + xlab("Chromosome Position (Mbp)") + ylab("-log10(p-value)") +
+      values = c("red", "darkorange1", "green1", "skyblue1", "navyblue", "grey")) +
+    theme_bw() + xlab(paste0("Chromosome ", chr_2, " Position (Mbp)")) + ylab("-log10(p-value)") +
     theme(legend.position = "bottom") +
     #annotate("text", x=((min(in.dt.2$CHR_POS)+max(in.dt.2$CHR_POS))/5), y=(max(in.dt.2$LOG10P)-0.1), label = name2, size = 4)+
-    xlim(min(in.dt.2$CHR_POS),max(in.dt.2$CHR_POS)) + ylim(min(in.dt.2$LOG10P),max(in.dt.2$LOG10P))
+    xlim(start,end) + ylim(min(in.dt.2$LOG10P),max(in.dt.2$LOG10P)) +
+    theme(axis.title.x=element_blank(),
+                        axis.text.x=element_blank(),
+                        axis.ticks.x=element_blank())
 
-
-  ggpubr::ggarrange(a, b, nrow = 2, ncol = 1,
-                    common.legend = TRUE, legend = "right", heights = 1)
+  #c = ggplot2::ggplot(genes, aes())
+  ggpubr::ggarrange(a, b, c, heights = c(2,2,1), nrow = 3, ncol = 1,
+                    common.legend = TRUE, legend = "right")
 }
 
 
--- R/single_plot.R
+++ R/single_plot.R
@@ -1,17 +1,67 @@
 #' Single Plot Function
 #'
-#' This function allows you to creat a plot of -log10(P-values).
-#' @param assoc_data a data frmae that has columns named CHR_POS representing
+#' This function allows you to creat a plot of -log10(P-values) of an association study
+#' by their genomic position, for example, the results of a GWAS or eQTL study. Sources
+#' 1000K genomes phase III data for linkage disequilibrium calculations.
+#' @param assoc_data required. a data frmae that has columns named CHR_POS representing
 #' the position of the snp on the chromosome, LOG10P which contains the
 #' -log10(P-values), and optional columns CHR, which contains the chromosome
 #' number, RS_ID, which contains the rsID numbers of the SNPs, and R2, which
 #' contains LD information.
-#' @keywords association plot
+#' @param rs_id optional. a character string. the rs_id number of the lead snp used for LD calculations, set NULL if input has LD information
+#' @param pops optional. populations from the 1000K Genomes dataset to use for LD calculations, set NULL if input has LD information
+#' @param start required. starting chromosomal position of the association plot
+#' @param end required. ending chromosomal position of the association plot
+#' @keywords association plot, gwas, linkage disequilibrium.
 #' @export
 #' @examples
-#' single_plot_function()
+#' single_plot_function(assoc_data = assoc_data, rs_id = "rs00000001", pops = c("CEU","TSI","FIN","GBR","IBS"), start = 1000000, end = 1230000)
 
-single_plot_function <- function(assoc_data){
+single_plot_function <- function(assoc_data, rs_id, pops, start, end){
+  print("Calculating LD")
+  if(!is.null(pops)){
+    if(length(pops) == 1){
+      ld_command = paste0("curl -k -X GET 'https://analysistools.nci.nih.gov/LDlink/LDlinkRest/ldproxy?var=", rs_id,
+                          "&pop=",pops,"&r2_d=r2'")
+      z = system(ld_command, intern = TRUE)
+      z = as.data.frame(z)
+      z = tidyr::separate(z, 1, into = c("col_1", "col_2", "col_3", "col_4", "col_5", "col_6", "col_7", "col_8",
+                                         "col_9", "col_10"), sep = "\t")
+      colnames(z) = z[1,]
+      z = z[-1,]
+      z = dplyr::select(z, RS_Number, R2)
+      colnames(z) = c("RS_ID", "LD")
+      assoc_data$LD = NA
+      assoc_data = dplyr::select(assoc_data, -LD)
+      assoc_data = merge(assoc_data, z, by = "RS_ID", all.x = TRUE)
+    }else if(length(pops) > 1){
+      ld_command = paste0("curl -k -X GET 'https://analysistools.nci.nih.gov/LDlink/LDlinkRest/ldproxy?var=", rs_id,
+                        "&pop=")
+      for (i in 1:length(pops)){
+        if (i < length(pops)){
+          print(i)
+          a = pops[i]
+          ld_command = paste0(ld_command, a, "%2B")
+        } else if(i == length(pops)){
+          a = pops[i]
+          ld_command = paste0(ld_command, a)
+          }
+        }
+      ld_command = paste0(ld_command, "&r2_d=r2'")
+      z = system(ld_command, intern = TRUE)
+      z = as.data.frame(z)
+      z = tidyr::separate(z, 1, into = c("col_1", "col_2", "col_3", "col_4", "col_5", "col_6", "col_7", "col_8",
+                                         "col_9", "col_10"), sep = "\t")
+      colnames(z) = z[1,]
+      z = z[-1,]
+      z = dplyr::select(z, RS_Number, R2)
+      colnames(z) = c("RS_ID", "LD")
+      assoc_data$LD = NA
+      assoc_data = dplyr::select(assoc_data, -LD)
+      assoc_data = merge(assoc_data, z, by = "RS_ID", all.x = TRUE)
+    }
+  }
+  print("Formatting Data")
   in.dt <- as.data.frame(assoc_data)
   in.dt$CHR_POS = as.numeric(as.character(in.dt$CHR_POS))
   in.dt$LOG10P = as.numeric(as.character(in.dt$LOG10P))
@@ -22,9 +72,39 @@ single_plot_function <- function(assoc_data){
   in.dt$LD_BIN = as.character(in.dt$LD_BIN)
   in.dt$LD_BIN[is.na(in.dt$LD_BIN)] <- "NA"
   in.dt$LD_BIN = as.factor(in.dt$LD_BIN)
-  ggplot2::ggplot(data = in.dt, aes(x = CHR_POS, y = LOG10P, color = LD_BIN)) +
+  in.dt = dplyr::filter(in.dt, CHR_POS > start)
+  in.dt = dplyr::filter(in.dt, CHR_POS < end)
+  print("Generating Plot")
+
+  # reading in gene data
+  data(biomart_hg19)
+  colnames(biomart_hg19) = c("GENE_ID", "CHR", "TRX_START", "TRX_END", "GENE_NAME", "LENGTH")
+  dat_chr = in.dt$CHR[1]
+  gene = subset(biomart_hg19, biomart_hg19$CHR == dat_chr)
+  gene = subset(gene, gene$TRX_START > (start-5000))
+  gene = subset(gene, gene$TRX_END < (end+5000))
+  gene = arrange(gene, desc(LENGTH))
+  gene = gene[!duplicated(gene$GENE_ID),]
+  gene = gene[,c(3,4,5)]
+  gene = reshape2::melt(gene)
+  gene$y_value = as.numeric(as.factor(gene$GENE_NAME))
+  plot_lab = subset(gene, gene$variable == "TRX_END")
+  c = ggplot(gene, aes(x = value, y = y_value)) +
+    geom_line(aes(group = GENE_NAME), size = 2) + theme_bw() +
+    geom_text(data = plot_lab, aes(x = value, y = y_value, label = GENE_NAME),
+              hjust = -0.1,vjust = 0.3, size = 2.5) + xlim(start,end) +
+    theme(axis.title.y = element_text(color = "white", size = 28),
+          axis.text.y = element_blank(),
+          axis.ticks.y = element_blank()) + xlab(paste0("Chromosome ", dat_chr, " Position")) +
+    ylim(0,(max(gene$y_value)+1))
+
+  b = ggplot2::ggplot(data = in.dt, aes(x = CHR_POS, y = LOG10P, color = LD_BIN)) +
     geom_point() + scale_colour_manual(
       values = c("navyblue", "skyblue1", "green1", "darkorange1", "red", "grey")) +
-    theme_bw() + xlab("Chromosome Position") + ylab("-log10(p-value)")
+    theme_bw() + xlab("Chromosome Position") + ylab("-log10(p-value)") +
+    xlim(start, end) + ylim(min(in.dt$LOG10P),max(in.dt$LOG10P))
+
+  ggpubr::ggarrange(b, c, heights = c(4,1), nrow = 2, ncol = 1,
+                    common.legend = TRUE, legend = "right")
 }
 
