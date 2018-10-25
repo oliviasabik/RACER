cdaaf8e3365f9cb2ff0a3f471b737a03739ad047
ols5fg <ols5fg@virginia.edu>
2018-10-25 13:50:32 -0400

    Adding hg19 data.

---
+++ R/biomart_hg38-data.R
@@ -0,0 +1,17 @@
+#'Biomart Gene Data
+#'
+#'Data from the Biomart database for all known genes
+#'
+#'@docType data
+#'
+#'@usage data(biomart_hg38)
+#'
+#'@format A flat file
+#'
+#'@keywords datasets
+#'
+#'@source \href{http://useast.ensembl.org/biomart/martview}
+#'
+#'@examples
+#'data(biomart_hg38)
+"biomart_hg38"
