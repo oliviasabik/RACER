f881a136ac3549bbe43accc060622b92b03af8c1
ols5fg <ols5fg@virginia.edu>
2018-10-31 15:33:25 -0400

    Adding example data

---
+++ R/mark3_bmd_gwas-data.R
@@ -0,0 +1,17 @@
+#'UKBB eBMD GWAS Data Release 2017
+#'
+#'Summary statistics for the 2017 GWAS of eBMD from the UK BioBank in the region containing the gene Mark3
+#'
+#'@docType data
+#'
+#'@usage data(mark3_bmd_gwas)
+#'
+#'@format A flat file
+#'
+#'@keywords datasets
+#'
+#'@source \href{http://www.gefos.org/?q=content/ukbb-ebmd-gwas-data-release-2017}
+#'
+#'@examples
+#'data(mark3_bmd_gwas)
+"mark3_bmd_gwas"
---
+++ R/mark3_eqtl-data.R
@@ -0,0 +1,17 @@
+#'GTEx data for Mark3 from thyroid tissue
+#'
+#'Data from the GTEx consortium, eQTL for Mark3 in thyroid
+#'
+#'@docType data
+#'
+#'@usage data(mark3_eqtl)
+#'
+#'@format A flat file
+#'
+#'@keywords datasets
+#'
+#'@source \href{https://gtexportal.org/home/}
+#'
+#'@examples
+#'data(mark3_eqtl)
+"mark3_eqtl"
